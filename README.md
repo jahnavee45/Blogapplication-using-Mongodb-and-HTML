d28dbc6882adf223c2f6ed51d11079f7b9a2ceaf
jahnavee45 <roysimran1545@gmail.com>
2024-06-05 09:53:30 +0530

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Blogapplication-using-Mongodb-and-HTML
+
+## This is a simple Blog Application, fetching your MongoDb database and displaying it through Index file
+1. Create an empty folder
+2. Install express, ejs and mongoose
+3. Create a server.js file and add your connection string provided by MongoDb
+4. In order to display your data, create a views folder and create a index.ejs file inside it
+5. Write the proper code to fetch data
+6. Create a public folder in the root and add a css folder in it to store the stylesheet
+7. Give style and run your localhost to see the results
---
+++ usingMonogodb/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+mongoose.connect('mongodb+srv://royjahnavee1545:yBdWtizsNBcAPMq4@cluster0.d3hqsxh.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const blog = mongoose.model('Post', blogSchema);
+
+app.get('/', async (req, res) => {
+    try {
+        const posts = await blog.find({});
+        
+        // Check if posts were found
+        if (posts.length === 0) {
+            return res.status(200).render('index', {
+                postList: []
+            });
+        }
+
+        res.render('index', {
+            postList: posts
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
+})
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
